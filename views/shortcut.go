7080544f451b6dcd3d3770682fce79c83f8862ac
justjcurtis <justjcurtis@gmail.com>
2024-10-31 11:58:37 +0000

    chore: cleanup shortcuts key to populate and use map

--- views/shortcut.go
+++ views/shortcut.go
@@ -25,45 +25,18 @@ func ShortcutKey() *fyne.Container {
 	title := canvas.NewText("Shortcuts", color.White)
 	title.TextSize = 20
 
-	quitTitle := canvas.NewText("Quit", color.White)
-	quitTitle.TextStyle = fyne.TextStyle{Bold: true}
-	quitShortcut := canvas.NewText("Q/Escape", color.White)
-
-	clearTitle := canvas.NewText("Clear", color.White)
-	clearTitle.TextStyle = fyne.TextStyle{Bold: true}
-	clearShortcut := canvas.NewText("C", color.White)
-
-	playpauseTitle := canvas.NewText("Play/Pause", color.White)
-	playpauseTitle.TextStyle = fyne.TextStyle{Bold: true}
-	playpauseShortcut := canvas.NewText("Space", color.White)
-
-	prevnextTitle := canvas.NewText("Prev/Next", color.White)
-	prevnextTitle.TextStyle = fyne.TextStyle{Bold: true}
-	prevnextShortcut := canvas.NewText("Left/Right", color.White)
-
-	delayTitle := canvas.NewText("Delay 0.5 +/-", color.White)
-	delayTitle.TextStyle = fyne.TextStyle{Bold: true}
-	delayShortcut := canvas.NewText("Up/Down", color.White)
-
-	shuffleTitle := canvas.NewText("Shuffle", color.White)
-	shuffleTitle.TextStyle = fyne.TextStyle{Bold: true}
-	shuffleShortcut := canvas.NewText("S", color.White)
-
-	settingsTitle := canvas.NewText("Settings", color.White)
-	settingsTitle.TextStyle = fyne.TextStyle{Bold: true}
-	settingsShortcut := canvas.NewText("/", color.White)
+	textElements := container.NewGridWithColumns(2)
+	for k, v := range ShortcutMap {
+		title := canvas.NewText(k, color.White)
+		title.TextStyle = fyne.TextStyle{Bold: true}
+		text := canvas.NewText(v, color.White)
+		textElements.Add(title)
+		textElements.Add(text)
+	}
 
 	box := container.NewVBox(
 		container.NewCenter(title),
-		container.NewGridWithColumns(2,
-			quitTitle, quitShortcut,
-			clearTitle, clearShortcut,
-			playpauseTitle, playpauseShortcut,
-			prevnextTitle, prevnextShortcut,
-			delayTitle, delayShortcut,
-			shuffleTitle, shuffleShortcut,
-			settingsTitle, settingsShortcut,
-		),
+		textElements,
 	)
 	return box
 }
